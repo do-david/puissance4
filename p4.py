ee6fabd2f908b1675c6deca434bd4a07dc78a742
do-david <50655142+do-david@users.noreply.github.com>
2021-04-23 12:51:11 +0200

    implementation graphique v3

--- p4.py
+++ p4.py
@@ -202,73 +202,144 @@ def draw_board(board):
 				pygame.draw.circle(screen, YELLOW, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
 	pygame.display.update()
 
-def text_objects(text, font):
+def text_title(text, font):
     textSurface = font.render(text, True, black)
     return textSurface, textSurface.get_rect()
 
+def text_objects(text, font):
+    textSurface = font.render(text, True, white)
+    return textSurface, textSurface.get_rect()
+
+#affichage du menu qui retourne nb_player et le palier de difficulté
 def game_intro():
-    #retourne nb_player et le palier de difficulté
     res = []
+    np = 0
+    lvl = 0
     intro = True
-    startClicked = False
+    startButton = False
+    playersButton = False
     while intro:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+            #tous les blocks de buttons où l'utilisateur clique
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if width/2 <= mouse[0] <= width/2+150 and (height/2) <= mouse[1] <= (height/2)+150:
-                    print('start button clicked')
-                    startClicked = True
+                if width/2-75 <= mouse[0] <= width/2+75 and (height/2)-40 <= mouse[1] <= (height/2)+40:
+                    startButton = True
                     continue
-                if width/5 <= mouse[0] <= width/5+100 and (height/4) <= mouse[1] <= (height*3/4)+100 and startClicked:
-                    print('this is easy')
-                    res.append(random.randint(1,2))
+                if (width/5)-50 <= mouse[0] <= (width/5)+50 and (height*5/8)-20 <= mouse[1] <= (height*5/8)+20 and startButton:
+                    playersButton = True
+                    np = 0
+                    continue
+                if (width/2)-70 <= mouse[0] <= (width/2)+70 and (height*5/8)-20 <= mouse[1] <= (height*5/8)+20 and startButton:
+                    playersButton = True
+                    np = 1
+                    continue
+                if (width*4/5)-100 <= mouse[0] <= (width*4/5)+100 and (height*5/8)-20 <= mouse[1] <= (height*5/8)+20 and startButton:
                     intro = False
-                if width/2 <= mouse[0] <= width/2+100 and (height*3/4) <= mouse[1] <= (height*3/4)+100 and startClicked:
-                    print('this is medium')
-                    res.append(random.randint(3,4))
+                    np = 2
+                    break
+                if (width/5)-50 <= mouse[0] <= (width/5)+50 and (height*3/4)-25 <= mouse[1] <= (height*3/4)+25 and startButton and playersButton:
+                    lvl = random.randint(1,2)
                     intro = False
-                if width*4/5 <= mouse[0] <= width*4/5+100 and (height/4) <= mouse[1] <= (height*3/4)+100 and startClicked:
-                    print('this is hard')
-                    res.append(random.randint(5,6))
+                if (width/2)-70 <= mouse[0] <= width/2+70 and (height*3/4)-25 <= mouse[1] <= (height*3/4)+25 and startButton and playersButton:
+                    lvl = random.randint(3,4)
                     intro = False
-        if(startClicked):
-            smallText = pygame.font.Font('freesansbold.ttf',35)
-            Text1Surf, Text1Rect = text_objects("Easy", smallText)
-            Text2Surf, Text2Rect = text_objects("Medium", smallText)
-            Text3Surf, Text3Rect = text_objects("Hard", smallText)
-            Text1Rect.center = ((width/5),(height*3/4))
-            Text2Rect.center = ((width/2),(height*3/4))
-            Text3Rect.center = ((width*4/5),(height*3/4))
+                if (width*4/5)-50 <= mouse[0] <= (width*4/5)+50 and (height*3/4)-25 <= mouse[1] <= (height*3/4)+25 and startButton and playersButton:
+                    lvl = random.randint(5,6)
+                    intro = False
+        #afficher la ligne des adversaires après avoir cliqué sur start
+        if(startButton):
+            #affichage des rectangles pour les buttons
+            if (width/5)-50 <= mouse[0] <= (width/5)+50 and (height*5/8)-20  <= mouse[1] <= (height*5/8)+20:
+                pygame.draw.rect(screen,color_light,[(width/5)-50,(height*5/8)-20,100,40])
+            else:
+                pygame.draw.rect(screen,color_dark,[(width/5)-50,(height*5/8)-20,100,40])
+            if (width/2)-70 <= mouse[0] <= (width/2)+70 and  (height*5/8)-20 <= mouse[1] <= (height*5/8)+20:
+                pygame.draw.rect(screen,color_light,[(width/2)-70,(height*5/8)-20,140,40])
+            else:
+                pygame.draw.rect(screen,color_dark,[(width/2)-70,(height*5/8)-20,140,40])
+            if (width*4/5)-100 <= mouse[0] <= (width*4/5)+100 and  (height*5/8)-20 <= mouse[1] <= (height*5/8)+20:
+                pygame.draw.rect(screen,color_light,[(width*4/5)-100,(height*5/8)-20,200,40])
+            else:
+                pygame.draw.rect(screen,color_dark,[(width*4/5)-100,(height*5/8)-20,200,40])
+            #affichage du textes dans les buttons
+            vrSmallText = pygame.font.Font('freesansbold.ttf',20)
+            Text1Surf, Text1Rect = text_objects("AI vs AI", vrSmallText)
+            Text2Surf, Text2Rect = text_objects("Player vs AI", vrSmallText)
+            Text3Surf, Text3Rect = text_objects("Player 1 vs Player 2", vrSmallText)
+            Text1Rect.center = ((width/5),(height*5/8))
+            Text2Rect.center = ((width/2),(height*5/8))
+            Text3Rect.center = ((width*4/5),(height*5/8))
             screen.blit(Text1Surf, Text1Rect)
             screen.blit(Text2Surf, Text2Rect)
             screen.blit(Text3Surf, Text3Rect)
+            #affichage de la ligne des différentes difficultés de l'IA
+            if(playersButton):
+                #affichage des rectangles pour les buttons
+                if (width/5)-50 <= mouse[0] <= (width/5)+50 and (height*3/4)-25  <= mouse[1] <= (height*3/4)+25:
+                    pygame.draw.rect(screen,color_light,[(width/5)-50,(height*3/4)-25,100,50])
+                else:
+                    pygame.draw.rect(screen,color_dark,[(width/5)-50,(height*3/4)-25,100,50])
+                if (width/2)-70 <= mouse[0] <= (width/2)+70 and  (height*3/4)-25<= mouse[1] <= (height*3/4)+25:
+                    pygame.draw.rect(screen,color_light,[(width/2)-70,(height*3/4)-25,140,50])
+                else:
+                    pygame.draw.rect(screen,color_dark,[(width/2)-70,(height*3/4)-25,140,50])
+                if (width*4/5)-50 <= mouse[0] <= (width*4/5)+50 and  (height*3/4)-25 <= mouse[1] <= (height*3/4)+25:
+                    pygame.draw.rect(screen,color_light,[(width*4/5)-50,(height*3/4)-25,100,50])
+                else:
+                    pygame.draw.rect(screen,color_dark,[(width*4/5)-50,(height*3/4)-25,100,50])
+                #affichage des textes dans les buttons
+                smallText = pygame.font.Font('freesansbold.ttf',35)
+                Text4Surf, Text4Rect = text_objects("Easy", smallText)
+                Text5Surf, Text5Rect = text_objects("Medium", smallText)
+                Text6Surf, Text6Rect = text_objects("Hard", smallText)
+                Text4Rect.center = ((width/5),(height*3/4))
+                Text5Rect.center = ((width/2),(height*3/4))
+                Text6Rect.center = ((width*4/5),(height*3/4))
+                screen.blit(Text4Surf, Text4Rect)
+                screen.blit(Text5Surf, Text5Rect)
+                screen.blit(Text6Surf, Text6Rect)
         pygame.display.update()
         mouse = pygame.mouse.get_pos()
         screen.fill(white)
+        #affichage du titre
         largeText = pygame.font.Font('freesansbold.ttf',115)
-        mediumText = pygame.font.Font('freesansbold.ttf',65)
-        TextSurf, TextRect = text_objects("Puissance 4", largeText)
+        TextSurf, TextRect = text_title("Puissance 4", largeText)
         TextRect.center = ((width/2),(height/4))
         screen.blit(TextSurf, TextRect)
+        #affichage du button start
+        if width/2-70 <= mouse[0] <= width/2+70 and (height/2)-40 <= mouse[1] <= (height/2)+40:
+            pygame.draw.rect(screen,color_light,[(width/2)-75,(height/2)-40,150,80])
+        else:
+            pygame.draw.rect(screen,color_dark,[(width/2)-75,(height/2)-40,150,80])
+        mediumText = pygame.font.Font('freesansbold.ttf',65)
         ButtonSurf, ButtonRect = text_objects("Start", mediumText)
         ButtonRect.center = ((width/2),(height/2))
         screen.blit(ButtonSurf, ButtonRect)
-    return res
+    if(not lvl):
+        res.append(np)
+        return res
+    else:
+        res.append(np)
+        res.append(lvl)
+        return res
         
 pygame.display.set_caption('Puissance 4')
 black = (0,0,0)
 white = (255,255,255)
+color_light = (170,170,170)
+color_dark = (100,100,100)
 board = create_board()
 print_board(board)
 gameIsOver = False
 pygame.init()
 #lvl = -1
-nb_player = -1
+# nb_player = -1
 #0: AI vs AI || 1: Player vs AI || 2: Player vs Player
-while nb_player < 0 or nb_player > 2:
-	nb_player = int(input("Nombre de joueur :"))
+# while nb_player < 0 or nb_player > 2:
+# 	nb_player = int(input("Nombre de joueur :"))
 #Choix du niveau
 # while lvl < 1 or lvl > 6:
 # 	lvl = int(input("Choix du niveau de difficulte (1-6):"))
@@ -281,8 +352,15 @@ size = (width, height)
 RADIUS = int(SQUARESIZE/2 - 5)
 screen = pygame.display.set_mode(size)
 res = game_intro()
-lvl = res[0]
-print('lvl = ',lvl)
+print(res)
+if(len(res) <2 and res[0] == 2):
+    nb_player = res[0]
+    print('nb_player = ',nb_player)
+else:
+    nb_player = res[0]
+    print('nb_player = ',nb_player)
+    lvl = res[1]
+    print('lvl = ',lvl)
 draw_board(board)
 pygame.display.update()
 myfont = pygame.font.SysFont("monospace", 75)
